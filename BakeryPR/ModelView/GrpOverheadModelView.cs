bd9a6857080a6a4739e1d0a929c66a00d5fb4fc0
Olabode Aguda <olabode.aguda@interswitchgroup.com>
2017-08-01 12:16:55 +0100

    Update

--- BakeryPR/ModelView/GrpOverheadModelView.cs
+++ BakeryPR/ModelView/GrpOverheadModelView.cs
@@ -1,5 +1,7 @@
 ﻿using BakeryPR.DAO;
 using BakeryPR.Models;
+using BakeryPR.Utilities;
+using BakeryPR.Views;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -12,7 +14,88 @@ namespace BakeryPR.ModelView
 {
     public class GrpOverheadModelView : INotifyPropertyChanged
     {
-        public OverheadDetailsDao overheadDao
+        private OverheadDetailsGroup _overheaddetailGrp;
+
+        public OverheadDetailsGroup overheaddetailGrp
+        {
+            get { return _overheaddetailGrp; }
+            set
+            {
+                _overheaddetailGrp = value;
+                this.NotifyPropertyChanged("overheaddetailGrp");
+            }
+        }
+
+        private OverheadDetails _overheadDetail = new OverheadDetails();
+
+        public OverheadDetails overheadDetail
+        {
+            get { return _overheadDetail; }
+            set
+            {
+                _overheadDetail = value;
+                this.NotifyPropertyChanged("overheadDetail");
+            }
+        }
+
+        private bool _isAddoverhead = false;
+
+        public bool isAddoverhead
+        {
+            get { return _isAddoverhead; }
+            set
+            {
+                _isAddoverhead = value;
+                this.NotifyPropertyChanged("isAddoverhead");
+            }
+        }
+
+        public DelegateCommand<object> addOverheadCommand
+        {
+            get
+            {
+                return new DelegateCommand<object>((s) =>
+                {
+                    AddOverheadGrp addoverhead = new AddOverheadGrp();
+                    addoverhead.DataContext = this;
+                    addoverhead.ShowDialog();
+                });
+            }
+        }
+
+        public DelegateCommand<object> addCommand
+        {
+            get
+            {
+                return new DelegateCommand<object>(async (s) =>
+                {
+                    await Task.Run(() =>
+                    {
+                        try
+                        {
+                            if (String.IsNullOrEmpty(overheadDetail.groupName))
+                            {
+                                return;
+                            }
+
+                            int id = overheadDetailsDao.add(this.overheadDetail);
+                            if (id > 0)
+                            {
+                                this.isAddoverhead = true;
+                                this.overheadDetail.id = id;
+                            }
+                        }
+                        catch (Exception x)
+                        {
+                            MessageBox.Show(x.Message, "Update", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+
+                    });
+                });
+            }
+        }
+
+        public OverheadDetailsDao overheadDetailsDao
         {
             get
             {
@@ -32,8 +115,23 @@ namespace BakeryPR.ModelView
             }
         }
 
+        public OverheadDao overheadDao
+        {
+            get
+            {
+                return new OverheadDao();
+            }
+        }
 
         public List<OverheadDetails> GrpLst
+        {
+            get
+            {
+                return overheadDetailsDao.all();
+            }
+        }
+
+        public List<Overhead> overheadLst
         {
             get
             {
